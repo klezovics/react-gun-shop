db16dc72b2b69a06c2bf08982ef328ca37fb4c73
klezovich <arthur@trevor.io>
2022-10-21 13:20:12 +0200

    Fix

--- src/components/gunshop/BuyButton.js
+++ src/components/gunshop/BuyButton.js
@@ -1,10 +1,5 @@
 function BuyButton() {
 
-    function playAudio(url) {
-        console.log("Playing sound")
-        new Audio(url).play();
-    }
-
     return (
         <button
             className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"}
