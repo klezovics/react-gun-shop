854528543c79d9530df7a11bc4dce8d5b00764a1
klezovich <arthur@trevor.io>
2022-10-21 12:50:37 +0200

    Integrate tailwind

--- src/components/gunshop/Gunshop.js
+++ src/components/gunshop/Gunshop.js
@@ -3,6 +3,9 @@ import GunDetails from "./GunDetails";
 function Gunshop() {
   return (
     <div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
       <h1>Welcome to the gunshop!</h1>
       <GunDetails name="AK-47" price={1000} url={"/img/gunshop/ak-47.png"}/>
       <GunDetails name="M-16" price={10000} url={"/img/gunshop/m-16.png"}/>
