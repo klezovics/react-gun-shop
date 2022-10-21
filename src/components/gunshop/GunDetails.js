4a2f8dbd2e666a23e2a33a3f883b784b73b56bc8
klezovich <arthur@trevor.io>
2022-10-21 13:27:05 +0200

    Fix

--- src/components/gunshop/GunDetails.js
+++ src/components/gunshop/GunDetails.js
@@ -4,9 +4,9 @@ function GunDetails(props) {
     return (
         <div style={{border: '1px solid black', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <div>
-                <img src={props.url}/>
+                <img src={props.url} style={{width: '200px', height: '200px'}}/>
             </div>
-            <div>
+            <div style={{marginLeft: '20px'}}>
               <p>Name: {props.name}</p>
               <p>Price: {props.price}</p>
               <BuyButton/>
